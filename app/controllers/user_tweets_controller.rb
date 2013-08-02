6087581627fd05b3c4a28ba0297e685d4f4112d8
suzuki_takuro <takuro.suzuki@drecom.co.jp>
2013-08-02 09:25:19 +0900

    不具合修正

--- app/controllers/user_tweets_controller.rb
+++ app/controllers/user_tweets_controller.rb
@@ -2,7 +2,12 @@ class UserTweetsController < ApplicationController
   # GET /user_tweets
   # GET /user_tweets.json
   def index
-    @user_tweets = UserTweet.all
+    @our_user_tweets = UserTweet.all
+    @user_tweets = []
+    user_follow = Relation.where(following_id: session[:login])
+    user_follow.each do |login_followed_id|
+      @user_tweets << UserTweet.where(user_id: login_followed_id)
+    end
     @users = User.all
     @relations = Relation.all
 
